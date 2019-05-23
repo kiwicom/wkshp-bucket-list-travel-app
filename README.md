d9345a0250e5cba32ca4f138e4df526dbe78ae39
Robin Cussol <me@robincussol.com>
2019-05-23 15:07:08 +0200

    Step 0

---
+++ README.md
@@ -0,0 +1,14 @@
+# Step 0
+
+## Intro to React Native
+
+**Goal**: Turn `DashedBox` and `Stopover` components in `apps/core/search/Search` look like:
+
+`DashedBox`
+![](./docs/assets/stopover-initial.png)
+
+`Stopover`
+![](./docs/assets/stopover-final.png)
+
+Context:
+![](./docs/assets/screenshot-step0.png)
--- packages/components/src/dashedBox/DashedBox.js
+++ packages/components/src/dashedBox/DashedBox.js
@@ -2,40 +2,16 @@
 
 import * as React from 'react';
 import { View } from 'react-native';
-import { Icon, Text, StyleSheet } from '@kiwicom/universal-components';
-import { defaultTokens } from '@kiwicom/orbit-design-tokens';
 
-const GREY = '#999';
+// Find an Icon component in '@kiwicom/universal-components'
+// It accepts a name props with value "plus"
 
 type Props = {|
-  +textContent: string,
+  // TODO
 |};
 
-const DashedBox = ({ textContent }: Props) => {
-  return (
-    <View style={styles.container}>
-      <Icon name="plus" color={GREY} />
-      <Text style={styles.text} size="large">
-        {textContent}
-      </Text>
-    </View>
-  );
+const DashedBox = (props: Props) => {
+  return <View>{/** TODO */}</View>;
 };
-
-const styles = StyleSheet.create({
-  container: {
-    padding: parseInt(defaultTokens.spaceMedium, 10),
-    borderWidth: 2,
-    borderRadius: 2,
-    borderColor: GREY,
-    borderStyle: 'dashed',
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  text: {
-    marginStart: 4,
-    color: GREY,
-  },
-});
 
 export default DashedBox;
